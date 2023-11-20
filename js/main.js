46e32370d1df4435c6c5e7d0848b9cf64b310019
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-20 17:53:34 +0100

    feat: milestone 0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 'use strict';
+const membroTeam = [
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    img: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    img: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    img: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    img: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    img: 'scott-estrada-developer.jpg',
+  },
+  {
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    img: 'barbara-ramos-graphic-designer.jpg',
+  },
+];
